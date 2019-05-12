543ac423c15881d838c74e42a7e59566bac5f1db
Gerald Bryant <hakeem1106@gmail.com>
2019-05-12 20:57:11 +0000

    add

--- fun.js
+++ fun.js
@@ -1,1 +1,33 @@
+const readline = require('readline');
+const arr = [];
+
+
+
+const makePer = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+Perform = ()=>{
+    do(
+        makePer.question("Enter a number: ", (answer)=>{
+    arr.push(makePer.input);
+
+    console.log(arr);
+
+//makePer.close();
+
+})
+
+    )
+    while(arr.length<9);
+
+}
+
+Perform();
+
+
+
+
+
 
